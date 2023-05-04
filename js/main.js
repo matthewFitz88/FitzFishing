f712d104069bd6ef02aef5c315168dbc3b808adb
professorguava <90649318+matthewFitz88@users.noreply.github.com>
2023-05-03 23:37:05 -0600

    2018

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-  
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-  
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-  
-      });
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  $navbarBurgers.forEach(el => {
+    el.addEventListener('click', () => {
+
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
     });
-  
   });
+
+});
+
+// slideshow code
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("bestCatches");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
